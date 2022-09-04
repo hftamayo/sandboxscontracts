c6219b97ba9d50abb6b20f55edef9cb98b4f3b26
hftamayo <hftamayo@gmail.com>
2022-09-04 07:29:25 -0600

    changing name to modifier function

--- exercises/04modifiers/Balance.sol
+++ exercises/04modifiers/Balance.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.7;
 
 contract UserBalance {
-   uint256 constant FEE = 3;
+   uint256 private constant FEE = 3;
     mapping (address => uint) private balances;
     address public owner;
 
@@ -71,7 +71,7 @@ contract UserBalance {
         balances[msg.sender] -= _amount;
     }
 
-    modifier checkPrevDeposit(address _addr){
+    modifier checkAddFundConstraints(address _addr){
         require(donorStructs[_addr].donorName.length == 0, "Donor needs to have a donation previously");
         require(donorStructs[_addr].donorAmount == 0, "The balance is zero. Make a deposit first");
         _;
