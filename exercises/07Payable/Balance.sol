4ac5424558965faa20adf52a9bc0a70027169ee2
hftamayo <hftamayo@gmail.com>
2022-09-05 11:39:46 -0600

    payable assignment: receive ether as deposit

---
+++ exercises/07Payable/Balance.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.7;
+
+contract UserBalance {
+    uint8 private clientCount;
+    mapping (address => uint256) private balances;
+    address public owner;
+
+  // Log the event about a deposit being made by an address and its amount
+    event LogDepositMade(address indexed accountAddress, uint amount);
+
+    // Constructor is "payable" so it can receive the initial funding of 30, 
+    // required to reward the first 3 clients
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
+
+    /// @notice Deposit ether into bank, requires method is "payable"
+    /// @return The balance of the user after the deposit is made
+    function deposit() public payable returns (uint256) {
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+        return balances[msg.sender];
+    }
+
+
+    /// @return The balance of the Simple Bank contract
+    function depositsBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
